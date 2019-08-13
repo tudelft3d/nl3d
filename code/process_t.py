6ecc7b8f31b12829a73da1c02e3c6beae9f14cfc
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-08-13 14:21:12 +0200

    VegetatieObject ✅

---
+++ code/process_t.py
@@ -0,0 +1,32 @@
+
+import json
+import uuid
+import copy
+
+filename = '../data/rotterdam/cityjson/t.json'
+fin = open(filename)
+j = json.loads(fin.read())
+j2 = copy.deepcopy(j)
+
+
+j2['extensions'] = {'NL3D': {'url': 'https://github.com/hugoledoux/nl3d/blob/master/schemas/extensions/nl_3d.json', 'version': '0.1'}}
+j2['metadata']['referenceSystem'] = 'urn:ogc:def:crs:EPSG::7415'
+
+
+uids = set()
+for cid in j['CityObjects']:
+    pos = cid.rfind('_')
+    uids.add(cid[:pos])
+# print(uids)
+
+for uid in uids:
+    j2['CityObjects'][uid + '_k']['geometry'].append(j2['CityObjects'][uid + '_s']['geometry'][0])
+    j2['CityObjects'][uid + '_k']['geometry'].append(j2['CityObjects'][uid + '_w']['geometry'][0])
+    j2['CityObjects'][uid + '_k']['type'] = '+VegetatieObject'
+    del j2['CityObjects'][uid + '_s']
+    del j2['CityObjects'][uid + '_w']
+
+json_str = json.dumps(j2, indent=2)
+fout = open('tmp.json', 'w')
+fout.write(json_str)
+
