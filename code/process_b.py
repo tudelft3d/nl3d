7bb31ba78741be786bb5599907cca7b2f3f887bc
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-09-25 16:21:50 +0200

    With the new simplified +Pand w/o all the stuff.
    I'll stick to what exists, wasting too much time here

--- code/process_b.py
+++ code/process_b.py
@@ -19,72 +19,28 @@ j2['@context'].append("http://localhost:8080/contexts/context_cityjson.jsonld")
 
 for cid in j['CityObjects']:
     co = j['CityObjects'][cid]
-    
     if (co['type'] == 'Building'):
-        #-- case 1
-        if ('children' not in co):
-            # print ('NO children')
-            j2['CityObjects'][cid]['type'] = '+Pand'
-            newid = str(uuid.uuid4())
-            j2['CityObjects'][newid] = {'type': '+Gebouw'}
-            j2['CityObjects'][newid]['geometry'] = []
-            j2['CityObjects'][newid]['children'] = [cid]
-            j2['CityObjects'][cid]['parents'] = [newid]
-            j2['CityObjects'][newid]['address'] = j['CityObjects'][cid]['address']        
-            del j2['CityObjects'][cid]['address']
+        j2['CityObjects'][cid]['type'] = '+Pand'
+        j2['CityObjects'][cid]['@context'] = 'https://bag.basisregistraties.overheid.nl/bag/id/pand/' + cid[3:]
+        if 'children' in j['CityObjects'][cid]:
+            for childid in j['CityObjects'][cid]['children']:
+                for each in j['CityObjects'][childid]['geometry']:
+                    j2['CityObjects'][cid]['geometry'].append(each)
 
-        #-- case 2: has children
-        else:
-            # print(cid)
-            if len(j['CityObjects'][cid]['geometry']) == 0:
-                j2['CityObjects'][cid]['type'] = '+Gebouw'
-                if 'children' in j['CityObjects'][cid]: 
-                    for childid in j['CityObjects'][cid]['children']:
-                        if (j2['CityObjects'][childid]['type'] == 'BuildingPart'):
-                            j2['CityObjects'][childid]['type'] = '+Pand'
-                        elif (j2['CityObjects'][childid]['type'] == 'BuildingInstallation'):
-                            j2['CityObjects'][childid]['type'] = '+PandInstallatie'
-                            # j2['CityObjects'][childid]['parents'] = [c]
-            else:
-                j2['CityObjects'][cid]['type'] = '+Pand'
-                newid = str(uuid.uuid4())
-                j2['CityObjects'][newid] = {'type': '+Gebouw'}
-                j2['CityObjects'][newid]['geometry'] = []
-                j2['CityObjects'][newid]['children'] = [cid]
-                j2['CityObjects'][cid]['parents'] = [newid]
-                j2['CityObjects'][newid]['address'] = j['CityObjects'][cid]['address']        
-                del j2['CityObjects'][cid]['address']
-                if 'children' in j['CityObjects'][cid]: 
-                    for childid in j['CityObjects'][cid]['children']:
-                        if (j2['CityObjects'][childid]['type'] == 'BuildingPart'):
-                            j2['CityObjects'][childid]['type'] = '+Pand'
-                        elif (j2['CityObjects'][childid]['type'] == 'BuildingInstallation'):
-                            j2['CityObjects'][childid]['type'] = '+PandInstallatie'
-                            j2['CityObjects'][childid]['parents'] = [cid]
-    
-
-    
-    # if (co['type'] == 'BuildingPart'):
-    #     if ('parents' not in co):
-    #         print('->', cid)
-    #     else:
-    #         print(cid)
-
+#-- delete BuildingPart and BuildingInstallation
+lsids = []
 for cid in j2['CityObjects']:
     co = j2['CityObjects'][cid]
     if (co['type'] == 'BuildingInstallation'):
-        j2['CityObjects'][cid]['type'] = '+PandInstallatie'
-
-for cid in j2['CityObjects']:
-    co = j2['CityObjects'][cid]
-    if (co['type'] == '+Gebouw'):
-        j2['CityObjects'][cid]['toplevel'] = True
-    else:
-        j2['CityObjects'][cid]['toplevel'] = False
-
+        lsids.append(cid)
+    if (co['type'] == 'BuildingPart'):
+        lsids.append(cid)
+for each in lsids:
+    del j2['CityObjects'][each]
 
 
 json_str = json.dumps(j2, indent=2)
 fout = open('tmp.json', 'w')
 fout.write(json_str)
+print('Done: tmp.json saved.')
 
