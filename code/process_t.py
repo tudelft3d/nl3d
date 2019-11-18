8d6ea3ae657c02cf63f093be640b82dc420da098
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-11-18 11:40:51 +0100

    Update script to process trees

--- code/process_t.py
+++ code/process_t.py
@@ -9,7 +9,7 @@ j = json.loads(fin.read())
 j2 = copy.deepcopy(j)
 
 
-j2['extensions'] = {'NL3D': {'url': 'https://github.com/hugoledoux/nl3d/blob/master/schemas/extensions/nl_3d.json', 'version': '0.1'}}
+
 j2['metadata']['referenceSystem'] = 'urn:ogc:def:crs:EPSG::7415'
 
 j2['@context'] = []
@@ -25,13 +25,14 @@ for cid in j['CityObjects']:
 for uid in uids:
     j2['CityObjects'][uid + '_k']['geometry'].append(j2['CityObjects'][uid + '_s']['geometry'][0])
     j2['CityObjects'][uid + '_k']['geometry'].append(j2['CityObjects'][uid + '_w']['geometry'][0])
-    j2['CityObjects'][uid + '_k']['type'] = '+VegetatieObject'
-    j2['CityObjects'][uid + '_k']['imgeo_type'] = 'boom'
-    j2['CityObjects'][uid + '_k']['toplevel'] = True
+    j2['CityObjects'][uid + '_k']['type'] = 'SolitaryVegetationObject'
+    j2['CityObjects'][uid + '_k']['attributes']['class'] = {}
+    j2['CityObjects'][uid + '_k']['attributes']['class'] = "VegetatieObject"
+    j2['CityObjects'][uid + '_k']['attributes']['function'] = "Boom"
     del j2['CityObjects'][uid + '_s']
     del j2['CityObjects'][uid + '_w']
 
-json_str = json.dumps(j2, indent=2)
+json_str = json.dumps(j2)
 fout = open('../data/rotterdam/cityjson/t_nl3d.json', 'w')
 fout.write(json_str)
 print('Done.')
