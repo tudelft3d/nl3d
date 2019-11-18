efa19bf62cf8fdbce281d99bcef5d06fbd6b875b
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-11-18 10:56:00 +0100

    Remove the nl3d extensions and use English/original names
    the "class" and "function" are used to identify the IMGeo class/type

--- code/process_b.py
+++ code/process_b.py
@@ -9,7 +9,7 @@ j = json.loads(fin.read())
 j2 = copy.deepcopy(j)
 
 
-j2['extensions'] = {'NL3D': {'url': 'https://github.com/hugoledoux/nl3d/blob/master/schemas/extensions/nl_3d.json', 'version': '0.1'}}
+# j2['extensions'] = {'NL3D': {'url': 'https://github.com/hugoledoux/nl3d/blob/master/schemas/extensions/nl_3d.json', 'version': '0.1'}}
 j2['metadata']['referenceSystem'] = 'urn:ogc:def:crs:EPSG::7415'
 
 j2['@context'] = []
@@ -20,8 +20,7 @@ j2['@context'].append("http://localhost:8080/contexts/context_cityjson.jsonld")
 for cid in j['CityObjects']:
     co = j['CityObjects'][cid]
     if (co['type'] == 'Building'):
-        j2['CityObjects'][cid]['type'] = '+Pand'
-        j2['CityObjects'][cid]['toplevel'] = True
+        j2['CityObjects'][cid]['attributes']['class'] = "Pand"
         j2['CityObjects'][cid]['@context'] = 'https://bag.basisregistraties.overheid.nl/bag/id/pand/' + cid[3:]
         if 'children' in j['CityObjects'][cid]:
             for childid in j['CityObjects'][cid]['children']:
@@ -41,7 +40,8 @@ for each in lsids:
     del j2['CityObjects'][each]
 
 
-json_str = json.dumps(j2, indent=2)
+# json_str = json.dumps(j2, indent=2)
+json_str = json.dumps(j2)
 fout = open('../data/rotterdam/cityjson/b_nl3d.json', 'w')
 fout.write(json_str)
 print('Done.')
